759a9405706a6d6e6b22e3317161dbbfeeaa9b3e
VarshaShetty28 <varshashetty551@gmail.com>
2025-03-14 21:34:20 +0530

    Added all pages basic setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Docters from './pages/Docters'
+import Login from './pages/Login'
+import MyAppointment from './pages/MyAppointment'
+import MyProfile from './pages/MyProfile'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='mx-0 sm:mx-[10%] w-100%'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/docters' element={<Docters/>}/>
+        <Route path='/docters/:speciality' element={<Docters/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/myappointment' element={<MyAppointment/>}/>
+        <Route path='/myprofile' element={<MyProfile/>}/>
+        <Route path='/appointment/:docId' element={<Appointment/>}/>
+      </Routes>
     </div>
   )
 }
